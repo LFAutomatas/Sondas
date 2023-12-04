310f7eef707dcc37e79dd1fdcd14c9f32f76fb78
Martin-Paez <mpaez.ok@gmail.com>
2023-12-04 03:20:38 -0300

    Entrega

--- primer-parte/Main.cpp
+++ primer-parte/Main.cpp
@@ -8,12 +8,21 @@ void readKey();
 int main()
 {
   Matrix matrix("data.txt", 5);
-  matrix.floydWarshall();
-  matrix.floydWarshall();
-  matrix.floydWarshall();
-  matrix.floydWarshall();
-  matrix.floydWarshall();
+  printf("Parte 1:\n\n");
+  printf("1) Realice la matriz de adyacencia y calcule la matriz de distancias minimas implementando el algoritmo de FLoyd en Lenguaje C. La implementacion del algoritmo debe ser escalable a k nodos.\n");
+  printf("\nRespuesta:\nMatriz adyacencia:\n");
   matrix.printMatrix();
+  matrix.floydWarshall();
+  matrix.floydWarshall();
+  matrix.floydWarshall();
+  matrix.floydWarshall();
+  matrix.floydWarshall();
+  printf("Matriz de distancias minimas:\n");
+  matrix.printMatrix();
+  printf("\n2) Determine si se pueden realizar conexiones entre todos los nodos, menores a 500 metros, pasando por nodos intermedios.\n");
+  printf("\nRespuesta: \nEn el grafo /Sondas/primera-parte/doc/grafo.png se observa que se puede ir de 1 a 2 en 300m, de 2 a 4 en 420m y de 4 a 5 en 150m. Por lo tanto si es posible realizar conexciones.\n");
+  printf("\n3) Implemente una serie de aserciones para verificar la correctitud del código.\n");
+  printf("\nRespuesta:\nTest unitarios:\n");
   test(3);
   test(4);
   test(5);
--- segunda-parte/nodo-lora-sensores/nodo-lora-sensores/nodo-lora-sensores.ino
+++ segunda-parte/nodo-lora-sensores/nodo-lora-sensores/nodo-lora-sensores.ino
@@ -1,125 +1,50 @@
-                     /*----------------------------------------------------------------------------------------------------------------------------------*/
-// Macros para activar/desactivar distintas partes del codigo --> 0L desactiva, 1L activa                                         // |
-#define SENSORES_ENABLE     1L                                                                                                    // |
-#define LORA_ENABLE         0L                                                                                                    // | 
-#define RTC_ENABLE          0L                                                                                                    // |
-#define PRUEBAS_ENABLE      0L                                                                                                    // |                                                                                                       // |  
-#define TEMP_ENABLE         1L                                                                                                // |
-#define PH_ENABLE           1L  
-#define COND_ENABLE         1L
-#define TURB_ENABLE         1L                                                                                                  // |                                                                                                  // |  
-/*----------------------------------------------------------------------------------------------------------------------------------*/
-
-
-// rele doble: https://lastminuteengineers.com/two-channel-relay-module-arduino-tutorial/
-// electrovalvula: https://www.taloselectronics.com/blogs/tutoriales/valvula-solenoide
-
-#if RTC_ENABLE
-#include "RTClib.h"
-#endif
-
-#if LORA_ENABLE
-#include <SPI.h>           
-#include <LoRa.h>
-#endif
-
-#if TEMP_ENABLE
-//Librerias para sensor de temperatura DS18B20
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#endif
-//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-char i;
-int x,aux4;
-float aux1, aux2, aux3;
+#define TURBIDITY_PIN             A0
+#define PH_PIN                    A3
+#define TEMPERATURE_PIN           A4
+#define CONDUCTIVITY_PIN          A5
+#define LED_PIN                   13
+#define SPEAKER_PIN               3
 
-#if PH_ENABLE
- int decimalPrecision = 2;                   // decimal a mostrar en el Display & el Serial Monitor
-float PH;
- const byte pHpin = A3;// el sensor se conecta al pin A3.        
-// Constants:-
+#define SPEAKER_FRECUENCY         ( 220 * 1.059 )
+#define DECIMAL_PRINT_PRECISION   2
 
+#define HIGH_PH                   5
+#define MEDIUM_TEMPERATURE        4
+#define HIGH_TEMPERATURA          3
+#define HIGH_TURBIDITY            2
+#define HIGH_CONDUCTIVITY         1       
+#define NORMAL_VALUES             0
+#define INITIAL_STATE            -1
 
-#endif
-
-
-
-#if TEMP_ENABLE
-float temperatura;  
- 
-  const int oneWirePin = A4;  //Pin digital 7A4 para sensor de temperatura 
-OneWire oneWireBus(oneWirePin);
-DallasTemperature sensor(&oneWireBus);
-#endif
-
-#if COND_ENABLE
-float conductividad;  
-#define cond A5
-   
-#endif
-
-#if TURB_ENABLE
+OneWire oneWire(TEMPERATURE_PIN);
+DallasTemperature sensors(&oneWire);
+int state = INITIAL_STATE;
+float conductividad;
+float temperatura;
 float turbidez; 
-#define turb A0
-   
-#endif
-
-#if RTC_ENABLE
-// RTC_DS1307 rtc;
-
-//    + 5V -> VCC
-//    GND -> GND
-//    SCL -> A5
-//    SDA -> A4
-
-RTC_DS3231 rtc; // Creacion del objeto RTC
-String daysOfTheWeek[7] = { "Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado" };
-String monthsNames[12] = { "Enero", "Febrero", "Marzo", "Abril", "Mayo",  "Junio", "Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre" };
-#endif
-
-#if LORA_ENABLE
-// Modulo LoRa sx1278
-const int nodo=102;
-String outgoing;              // outgoing message
-String Mymessage = "";
-const int csPin = 10;          // LoRa radio chip select
-const int resetPin = 9;        // LoRa radio reset
-const int irqPin = 2;          // change for your board; must be a hardware interrupt pin
-#endif
-
-#if PRUEBAS_ENABLE
-
-int PH = 7;
-int temperatura = 23.0;
- 
-#endif
-
-
-/*********************** PERIFERICOS *****************************/
-
-#define PIN_LED             13
-#define PIN_SPEAKER         3
-#define SPEAKER_FRECUENCY   ( 220 * 1.059 )
+float PH;
 
 void ledOn()
 {
-  analogWrite(PIN_LED, 255);
+  digitalWrite(LED_PIN, 1);
 }
 
 void ledOff()
 {
-  analogWrite(PIN_LED, 0);
+  digitalWrite(LED_PIN, 0);
 }
 
 void speakerOn()
 {
-  tone(PIN_SPEAKER,SPEAKER_FRECUENCY);
+  tone(SPEAKER_PIN,SPEAKER_FRECUENCY);
 }
 
 void speakerOff()
 {
-  noTone(PIN_SPEAKER); 
+  noTone(SPEAKER_PIN);
 }
 
 void sendMessage(String topic, String message)
@@ -128,260 +53,255 @@ void sendMessage(String topic, String message)
   Serial.println(request);
 }
 
-/*********************** SETUP *****************************/
-
-void setup() 
+void onHighPh()
 {
-  // Setup Perifericos
-
-  pinMode(PIN_LED, OUTPUT);
-  Serial.begin(115200);
-
-
-  // Resto del Setup
-
-  //delay(1000); 
-
-#if LORA_ENABLE  
-   LoRa.setSPIFrequency(4.0E6);
-   LoRa.setPins(csPin, resetPin, irqPin);
-   if (!LoRa.begin(433E6)) {       
-      Serial.println("Falló inicialización de LoRa, comprobar conexiones");
-      while (true);                       // if failed, do nothing
-   }else{ Serial.println("LoRa inicializado");}
-   LoRa.setSyncWord(0xF3);
-#endif   
-   
-#if RTC_ENABLE  
-   if (!rtc.begin()) {
-      Serial.println(F("Couldn't find RTC"));
-      while (1);
-   }
-   delay(200);
-   // Si se ha perdido la corriente, fijar fecha y hora
-   if (rtc.lostPower()) {
-      // Fijar la fecha y hora de compilacion
-      rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); // Esto inicializará el RTC con el tiempo que compila el boceto, obviamente usando la fecha y hora de la PC.
-      
-      // Fijar a fecha y hora específica. En el ejemplo, 21 de Enero de 2016 a las 03:00:00
-      // rtc.adjust(DateTime(2016, 1, 21, 3, 0, 0));
-   }
-#endif
-
-#if SENSORES_ENABLE   
- 
-
-  #if PH_ENABLE
-   
-  #endif
-
-  
-#endif  
+  speakerOn();
+  ledOn();
+  sendMessage(
+    "alarmaPH",
+    "Verter solucion acida/alcalina para regular el PH."
+  );
 }
 
-#if RTC_ENABLE
-void printDate(DateTime date)
+void onLowPh()
 {
-   Serial.print(date.year(), DEC);
-   Serial.print('/');
-   Serial.print(date.month(), DEC);
-   Serial.print('/');
-   Serial.print(date.day(), DEC);
-   Serial.print(" (");
-   Serial.print(daysOfTheWeek[date.dayOfTheWeek()]);
-   Serial.print(") ");
-   Serial.print(date.hour(), DEC);
-   Serial.print(':');
-   Serial.print(date.minute(), DEC);
-   Serial.print(':');
-   Serial.print(date.second(), DEC);
-   Serial.println();
+  speakerOff();
+  ledOff();
+  sendMessage(
+    "alarmaPH",
+    "PH reestablecido a valores aceptables."
+  );
 }
-#endif 
 
-/*********************** LOOP *****************************/
-
-void loop() 
+void onMediumTemperature()
 {
-#if RTC_ENABLE  
-  // RTC: Obtener fecha actual y mostrar por Serial
-   DateTime now = rtc.now();
-   printDate(now);
-#endif
+  sendMessage(
+    "alertaTemp",
+    "Los niveles de temperatura actuales favorecen la reproducciOn de bacterias nocivas para la salud."
+  );
+}
 
-#if SENSORES_ENABLE   
- 
-  #if TEMP_ENABLE
-  //lectura de Temperatura
-       sensor.requestTemperatures();
-   sensor.getTempCByIndex(0);
-   temperatura=sensor.getTempCByIndex(0);
-   Serial.print(temperatura);
-    Serial.println("°C");
-     delay(100);   
-  #endif
-  
- 
-   
-   #if PH_ENABLE
-  // Lectura de PH
-  PH = (1023 - analogRead(pHpin)) / 73.07; // fórmula para leer PH entre 0-14.
-Serial.println(PH, decimalPrecision);// muestra el resultado en el serial monitor.
-delay(1000);// espera un segundo por lectura.
-   #endif
-
-  #if COND_ENABLE
-       
-aux1=0;
-for(i=0;i<100;i++)
+void onLowTemperature()
 {
-aux1=aux1+((analogRead(cond)*5.00)/1023.00);
-}
-aux1=aux1/100;
-aux1=aux1*1023/5.00;
-if(aux1>=927)
-conductividad=0;
-if((aux1<927) && (aux1>=838)){
-  conductividad=-30.0*(aux1-838)+3000;
+  sendMessage(
+    "alertaTemp",
+    "Los niveles de temperatura actuales ya No favorecen la reproducciOn de bacterias nocivas para la salud."
+  );
 }
 
-if((aux1>=800)&&(aux1<=837)){
-  conductividad=5800;
-}
-
-  if((aux1>=785)&&(aux1<800)){
-    conductividad=8100;
-}
- if((aux1>=769)&&(aux1<785)){
-    conductividad=11200;
-}
- if((aux1>=765)&&(aux1<769)){
-    conductividad=17500;
-}
- if((aux1>763)&&(aux1<765)){
-    conductividad=27000;
-}
- if((aux1>774)&&(aux1<=762)){
-    conductividad=30100;
-}
- if(aux1<=774){
-    conductividad=40200;
-}
-
-Serial.print(conductividad);
-    Serial.println("uS/cm");
-   #endif
-
-     #if TURB_ENABLE
-  // Lectura de turbidez
-  delay(100);
-aux3=0;
-for(i=0;i<100;i++)
+void onHighTemperature()
 {
-aux3=aux3+((analogRead(3)*5.00)/1023.00);
-}
-aux3= aux3/100;
-
-if(aux3 >= 3.20) {
-  turbidez=0.00;
-}
-if(aux3<3.20 && aux3>=0){
-  turbidez=-1093.75*aux3 + 3500.00;  
+  ledOn();
+  sendMessage(
+    "alertaTemp",
+    "Prohibir descarga de agua."
+  );
 }
 
-if(aux3<0.00){
-  turbidez=3500.00;
-}
-Serial.print(turbidez);
-Serial.println(" NTU");
-delay(1500);
-   #endif
-   
-#endif
-
-   #if LORA_ENABLE
-   // Envio de los valores mediante LoRa
-   delay(10);
-   Mymessage = String(nodo)+ "/" + String(Temp,3) + "&" + String(PH)+"#"+String(Turbidez);  
-   sendLoraMessage(Mymessage);
-   delay(10);
-   //Mymessage = " ";
-   #endif
-
-/* Con la porción de código que sigue se prueba el envio de datos entre modulos LoRa del NODO y GATEWAY enviando valores fijos.
-   En condiciones normales de funcionamiento se desactiva esta parte del codigo con 0L en el macro*/
-#if PRUEBAS_ENABLE
-  delay(10);
-  Mymessage = Mymessage + String(Temp) + "/" + String(PH)+ "&"+String(Turbidez)"\n";  
-  sendLoraMessage(Mymessage);
-  delay(100);
-  Mymessage = "";
-#endif
-
- //Esperar algunos segundos entre mediciones
-   delay(500);
-  
-  comprobarPh();
-  comprobarConductividad();
-  comprobarTurbidez();
-  comprobarTemperatura();
-}
-
-
-/*********************** AUTOMATAS *****************************/
-
-
-void comprobarPh()
+void onFromHighToMediumTemperature()
 {
+  ledOff();
+  sendMessage(
+    "alertaTemp",
+    "Volver a permitir la descarga de agua."
+  );
+}
+
+void onHighTurbidity()
+{
+  sendMessage(
+    "alertaExtraccionAgua",
+    "Extraer residuos."
+  );
+}
+
+void onLowTurbidity()
+{
+  sendMessage(
+    "alertaExtraccionAgua",
+    "Ya no es necesario extraer residuos."
+  );
+}
+
+void onHighConductivity()
+{
+  sendMessage(
+    "alertaConsumoAgua",
+    "Agua no apta para el consumo."
+  );
+}
+
+void onLowConductivity()
+{
+  sendMessage(
+    "alertaConsumoAgua",
+    "El agua ah vuelto a ser apta para el consumo."
+  );
+}
+
+void onNormalesValues()
+{
+}
+
+void leaveCurrentState()
+{
+  switch(state)
+  {
+    case HIGH_PH: onLowPh(); break;
+    case MEDIUM_TEMPERATURE: onLowTemperature(); break;
+    case HIGH_TEMPERATURA: onFromHighToMediumTemperature(); break;
+    case HIGH_TURBIDITY: onLowTurbidity(); break;
+    case HIGH_CONDUCTIVITY: onLowConductivity(); break;
+    default: break;
+  }
+}
+
+void changeState(int newState)
+{
+  if(newState < state)
+    leaveCurrentState();
+  state = newState;
+  switch(state)
+  {
+    case HIGH_PH: onHighPh(); break;
+    case MEDIUM_TEMPERATURE: onMediumTemperature(); break;
+    case HIGH_TEMPERATURA: onHighTemperature(); break;
+    case HIGH_TURBIDITY: onHighTurbidity(); break;
+    case HIGH_CONDUCTIVITY: onHighConductivity(); break;
+    default: break;
+  }
+}
+
+void automata()
+{
+  int newState = NORMAL_VALUES;
   if(PH < 6 || PH > 9)
-  {
-    speakerOn();
-    ledOn();
-    sendMessage(
-      "alarmaPH",
-      "Verter solución acida/alcalina para regular el ph"
-    );
-  }
+    newState = HIGH_PH;
+  else if(temperatura > 25 && temperatura < 35)
+    newState = MEDIUM_TEMPERATURE;
+  else if(temperatura > 35)
+    newState = HIGH_TEMPERATURA;
+  else if(turbidez > 2000)
+    newState = HIGH_TURBIDITY;
+  else if(conductividad > 1500)
+    newState = HIGH_CONDUCTIVITY;
   else
+    newState = NORMAL_VALUES;
+  if(newState != state)  
+    changeState(newState);
+}
+
+float readConductivity(void)
+{
+  char i;
+  int x,aux4;
+  float aux1, aux2, aux3;
+  aux1=0;
+  for(i=0;i<100;i++)
   {
-    speakerOff();
-    ledOff();
+  aux1=aux1+((analogRead(CONDUCTIVITY_PIN)*5.00)/1023.00);
   }
+  aux1=aux1/100;
+  aux1=aux1*1023/5.00;
+  if(aux1>=927)
+  conductividad=0;
+  if((aux1<927) && (aux1>=838)){
+    conductividad=-30.0*(aux1-838)+3000;
+  }
+
+  if((aux1>=800)&&(aux1<=837)){
+    conductividad=5800;
+  }
+
+    if((aux1>=785)&&(aux1<800)){
+      conductividad=8100;
+  }
+  if((aux1>=769)&&(aux1<785)){
+      conductividad=11200;
+  }
+  if((aux1>=765)&&(aux1<769)){
+      conductividad=17500;
+  }
+  if((aux1>763)&&(aux1<765)){
+      conductividad=27000;
+  }
+  if((aux1>774)&&(aux1<=762)){
+      conductividad=30100;
+  }
+  if(aux1<=774){
+      conductividad=40200;
+  }
+  return conductividad;
 }
 
-void comprobarConductividad()
+float readTurbidity(void)
 {
+  int x,aux4;
+  int i;
+  float aux1, aux2, aux3;
+  aux3=0;
+  for(i=0;i<100;i++)
+  {
+  aux3=aux3+((analogRead(TURBIDITY_PIN)*5.00)/1023.00);
+  }
+  aux3= aux3/100;
 
+  if(aux3 >= 3.20) {
+    turbidez=0.00;
+  }
+  if(aux3<3.20 && aux3>=0){
+    turbidez=-1093.75*aux3 + 3500.00;  
+  }
+
+  if(aux3<0.00){
+    turbidez=3500.00;
+  }
+  Serial.print("Turbidez: ");
+  Serial.print(turbidez);
+  Serial.println(" NTU");
+  return turbidez;
 }
 
-void comprobarTurbidez()
-{
+float Read_Temperature(void){
+  float tempC;
+  tempC = DEVICE_DISCONNECTED_C;
+  while(tempC == DEVICE_DISCONNECTED_C){
+    sensors.requestTemperatures(); // Send the command to get temperatures
+    tempC = sensors.getTempCByIndex(0);
+  }
+  return tempC;
+}
   
+float readPh(void)
+{// fórmula para leer PH entre 0-14.
+  return (1023 - analogRead(PH_PIN)) / 73.07;
 }
 
-#define INITIAL_STATE         0       
-#define NORMAL_TEMPERATURE    1
-#define ALERT_TEMPERATURE     2
-#define CRITICAL_TEMPERATURA  3
-
-int TemperatureState = NORMAL_TEMPERATURE
-void comprobarTemperatura() 
-{
-  switch(TemperatureState)
-  {
-    case NORMAL_TEMPERATURE :
-      break;
-    case ALERT_TEMPERATURE :
-      break;
-    case CRITICAL_TEMPERATURA :
-      break;
-  }
+void setup(void) {
+  Serial.begin(115200);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(SPEAKER_PIN, OUTPUT);
+  sensors.begin();
 }
 
-#if LORA_ENABLE
-void sendLoraMessage(String outgoing) {
-  LoRa.beginPacket();                    // start packet
-  LoRa.print(outgoing);                  // add payload
-  LoRa.endPacket(true);                      // finish packet and send it
+void loop(void) {
+
+  temperatura = Read_Temperature();
+  Serial.print("Temperatura: ");
+  Serial.print(temperatura);
+  Serial.println("^C");
+  PH = readPh();
+  Serial.print("PH: ");  
+  Serial.println(PH, DECIMAL_PRINT_PRECISION);
+  conductividad = readConductivity();
+  Serial.print("Conductividad: ");
+  Serial.print(conductividad);
+  Serial.println("uS/cm");
+  turbidez = readTurbidity();
+  Serial.print("Conductividad: ");
+  Serial.print(conductividad);
+  Serial.println("uS/cm");
+  automata();
+  delay(100);
+  Serial.println();
 }
-#endif
